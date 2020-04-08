54d7e8a997751e2a1a20bcf9d6cadd888dd87ac0
nevemore <neverov-ra@yandex.ru>
2020-04-08 19:17:28 +0300

    RAM

--- snake/Program.cs
+++ snake/Program.cs
@@ -6,19 +6,65 @@ using System.Threading.Tasks;
 
 namespace snake
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 1;
+			Func1(x);
+			Console.WriteLine("Call Func1. x = " + x);
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+			x = 1;
+			Func2(x);
+			Console.WriteLine("Call Func2. x = " + x);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+			x = 1;
+			Func3(x);
+			Console.WriteLine("Call Func3. x = " + x);
 
-            Console.ReadLine();
-        }
+			Point p1 = new Point(1, 3, '*');
+			Move(p1, 10, 10);
+			Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
 
-     }
+			Point p2 = new Point(4, 5, '#');
+			p1 = p2; //Присвоение адреса, а не значений. В значения по этому адресу записываем значения далее
+			p2.x = 8;
+			p2.y = 8;
+			Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+			p1 = new Point(1, 3, '*');
+			Update(p1); //Меняем адреса без изменения значений... Но Update не работает в том виде, в котором он описан в задании (ошибка без параметров)
+			Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+			Console.ReadLine();
+		}
+
+		public static void Func1(int value)
+		{
+
+		}
+
+		public static void Func2(int value)
+		{
+			value += 1;
+		}
+
+		public static void Func3(int x)
+		{
+			x += 1;
+		}
+
+		public static void Move(Point p, int dx, int dy)
+		{
+			p.x += dx;
+			p.y += dy;
+		}
+
+		public static void Update(Point p)
+		{
+			p = new Point(0, 0, '\0');
+		}
+
+
+	}
 }
