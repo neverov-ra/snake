7991f742582dff6fee37f3918261e99eda826fe1
nevemore <neverov-ra@yandex.ru>
2020-04-09 21:36:58 +0300

    Класс "Змейка", список, абстрагирование

--- snake/Program.cs
+++ snake/Program.cs
@@ -11,23 +11,33 @@ namespace snake
         static void Main(string[] args)
         {
 
+			Console.Title = "Snake";
 			Console.SetWindowSize(1, 1);
 			Console.SetBufferSize(80, 25);
 			Console.SetWindowSize(80, 25);
+			Console.ForegroundColor = ConsoleColor.Green;
 
 			// Отрисовка рамочки
 			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
 			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
 			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
 			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+			// В данном случае размер игрового окна будет зависеть от фактического размера консоли
+			/*
+			HorizontalLine upLine = new HorizontalLine(0, Console.BufferWidth - 1, 0, '*');
+			HorizontalLine downLine = new HorizontalLine(0, Console.BufferWidth - 1, Console.BufferHeight - 1, '');
+			VerticalLine leftLine = new VerticalLine(0, Console.BufferHeight - 1, 0, '');
+			VerticalLine rigthLine = new VerticalLine(0, Console.BufferHeight - 1, Console.BufferWidth - 1, '');
+			*/
 			upLine.Draw();
 			downLine.Draw();
 			leftLine.Draw();
 			rightLine.Draw();
 			
 			// Отрисовка точек
-			Point p = new Point(4, 5, '*');
-			p.Draw();
+			Point p = new Point(3, 11, '*');
+			Snake snake = new Snake(p, 7, Direction.DOWN);
+			snake.Draw();
 
 			Console.ReadKey();
 		
