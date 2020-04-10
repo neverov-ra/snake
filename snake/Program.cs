dc8baedc1f23b412cde85e86dace98b9b1d192d8
nevemore <neverov-ra@yandex.ru>
2020-04-10 21:26:11 +0300

    Полиморфизм

--- snake/Program.cs
+++ snake/Program.cs
@@ -9,65 +9,35 @@ namespace snake
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
+			VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+			Draw(vl);
 
-			Console.Title = "Snake";
-			Console.SetWindowSize(1, 1);
-			Console.SetBufferSize(80, 25);
-			Console.SetWindowSize(80, 25);
-			Console.ForegroundColor = ConsoleColor.Green;
+			Point p = new Point(4, 5, '*');
+			Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+			Draw(fSnake);
+			Snake snake = (Snake)fSnake;
 
-			// Отрисовка рамочки
-			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-			// В данном случае размер игрового окна будет зависеть от фактического размера консоли
-			/*
-			HorizontalLine upLine = new HorizontalLine(0, Console.BufferWidth - 1, 0, '*');
-			HorizontalLine downLine = new HorizontalLine(0, Console.BufferWidth - 1, Console.BufferHeight - 1, '');
-			VerticalLine leftLine = new VerticalLine(0, Console.BufferHeight - 1, 0, '');
-			VerticalLine rigthLine = new VerticalLine(0, Console.BufferHeight - 1, Console.BufferWidth - 1, '');
-			*/
-			upLine.Draw();
-			downLine.Draw();
-			leftLine.Draw();
-			rightLine.Draw();
-			
-			// Отрисовка точек
-			Point p = new Point(37, 12, '*');
-			Snake snake = new Snake(p, 3, Direction.RIGHT);
-			snake.Draw();
+			HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
 
-			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-			Point food = foodCreator.CreateFood();
-			food.Draw();
-
-			Console.CursorVisible = false;
-
-			while (true)
+			List<Figure> figures = new List<Figure>
 			{
-				if (snake.Eat(food))
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-				{
-					snake.Move();
-				}
+				fSnake,
+				vl,
+				hl
+			};
 
-				Thread.Sleep(200);
-
-				if (Console.KeyAvailable)
-				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey(key.Key);
-				}
+			foreach (var f in figures)
+			{
+				f.Draw();
 			}
-
 		}
 
-     }
+		static void Draw(Figure figure)
+		{
+			figure.Draw();
+		}
+
+	}
 }
